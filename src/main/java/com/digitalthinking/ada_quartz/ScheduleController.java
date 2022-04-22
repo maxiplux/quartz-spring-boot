3a61fdaa1127f22286e54d545f02af657efd1f6c
Juan Francisco Mosquera <maxiplux@gmail.com>
2022-04-22 07:47:48 -0400

    test 1

---
+++ src/main/java/com/digitalthinking/ada_quartz/ScheduleController.java
@@ -0,0 +1,34 @@
+package com.digitalthinking.ada_quartz;
+
+import org.quartz.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ScheduleController {
+
+    @Autowired
+    private Scheduler scheduler;
+
+    @GetMapping(value = "/schedule/{detail}/{desc}")
+    public String scheduleJob(@PathVariable(value = "detail") String detail, @PathVariable(value = "desc") String desc) throws SchedulerException {
+        JobDetail job = newJob(detail, desc);
+        return scheduler.scheduleJob(job, trigger(job)).toString();
+    }
+
+    private JobDetail newJob(String identity, String description) {
+        return JobBuilder.newJob().ofType(SimpleJob.class).storeDurably()
+                .withIdentity(JobKey.jobKey(identity))
+                .withDescription(description)
+                .build();
+    }
+
+    private SimpleTrigger trigger(JobDetail jobDetail) {
+        return TriggerBuilder.newTrigger().forJob(jobDetail)
+                .withIdentity(jobDetail.getKey().getName(), jobDetail.getKey().getGroup())
+                .withSchedule(SimpleScheduleBuilder.repeatSecondlyForever(1))
+                .build();
+    }
+}
